e66377ffbb28c93ef353f828d0a86eff1868b748
Samuel Moelius <sam.moelius@trailofbits.com>
2019-05-22 18:18:18 -0400

    Adding Apache 2.0 license.

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #======================================================================================================#
-# Copyright 2019 Trail of Bits. All rights reserved.
-# filter.sh
+# Copyright 2019 Trail of Bits
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.# Makefile for OnEdge
+#======================================================================================================#
 # This script applies some typical "filters" to the output of Go's race detector.  This script expects
 # its input to be in the form produced by normalize.py (a script accompanying this one).
 #
